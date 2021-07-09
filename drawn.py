5f6fe04fbec327004991113114b58373079209cd
Carlos Daniel Pohlod <carlospohlod@gmail.com>
2021-07-09 20:53:57 -0300

    adapted to iou calc

--- drawn.py
+++ drawn.py
@@ -5,19 +5,26 @@ from functions import *
 
 # ....................... Especific image ..................
 
-img = readImg('C:/image.jpg')
-coordinates = readCoordinates('C:/image.xml' )
-resultCoodinates  = readResults("C:/result_image.txt")
+# img = readImg('C:/Users/carlos/Documents/projetos/FAPA/results/dji_0575_quadrante2.jpg')
+# coordinates = readCoordinates('results/dji_0575_quadrante2.xml' )
+# resultCoodinates  = readResults("C:/Users/carlos/Documents/projetos/FAPA/results/result_dji_0575_quadrante2.txt")
 
-cv2 = drawnBoxes(img, coordinates)
-cv2 = drawnBoxes(img = img, coordinates = resultCoodinates, color = (0,255,0))
+# cv2 = drawnBoxes(img, coordinates, (0,0,255))
+# cv2 = drawnBoxes(img = img, coordinates = resultCoodinates, color = (0,255,0))
 
-cv2.imwrite('output.png', img)
+# #Calc IOU and dranw on the image
+# cv2, iou = calcIOU(labelCoordinates = coordinates, resultCoodinates = resultCoodinates, img = img, cv2 = cv2, drawn = True)
+# # OR
+# iou = calcIOU(labelCoordinates = coordinates, resultCoodinates = resultCoodinates)
+
+# writeFileInsights(coordinates, resultCoodinates, iou, 'dji_0575_quadrante2')
+
+# cv2.imwrite('output.png', img)
 
 
 #  ................... All images folder
 
-drawnAllFiles(imageBoxColor=(0,255,0), resultBoxColor=(0,255,0))
+drawnAllFiles(imageBoxColor=(0,0,255), resultBoxColor=(0,255,0), iouImageDrawn = True)
     
 
 
